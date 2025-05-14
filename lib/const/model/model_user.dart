06bd69bbd209ffa9549e7ed4b1fdcb382d9ea498
hoonk0 <karlhkim1@gmail.com>
2025-05-14 21:10:52 +0900

    모델 알람 코트 변경

--- lib/const/model/model_user.dart
+++ lib/const/model/model_user.dart
@@ -1,3 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../value/enum.dart';
+import '../value/keys.dart';
+
 class ModelUser {
   final String uid;
   final Timestamp dateCreate;
